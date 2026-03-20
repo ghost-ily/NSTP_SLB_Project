5f2f04786668f798b0d3a5bb90fc58f204c0a0a7
Leo Sibayan <rafaellosibayan36@gmail.com>
2026-03-21 06:13:23 +0800

    First prototype

--- Game 1 Resources/brick.gd
+++ Game 1 Resources/brick.gd
@@ -1,18 +1,24 @@
 extends RigidBody2D
 
 @onready var mouse_area: Area2D = $mouseArea
+@onready var under_ray: RayCast2D = $RayCast2D
+
 var has_mouse: bool = false
+var brick_moved: bool = false
+var under_object
 
 # Called when the node enters the scene tree for the first time.
 func _ready() -> void:
-	pass # Replace with function body.
+	mouse_area.mouse_shape_entered.connect(_on_mouse_area_mouse_shape_entered)
+	mouse_area.mouse_shape_exited.connect(_on_mouse_area_mouse_shape_exited)
 
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
+	under_object = under_ray.get_collider()
 	if Input.is_action_pressed("left_click") && has_mouse:
 		self.position = get_global_mouse_position()
-		
+		brick_moved = true
 
 
 func _on_mouse_area_mouse_shape_entered(shape_idx: int) -> void:
@@ -21,3 +27,8 @@ func _on_mouse_area_mouse_shape_entered(shape_idx: int) -> void:
 
 func _on_mouse_area_mouse_shape_exited(shape_idx: int) -> void:
 	has_mouse = false
+	brick_moved = false
+	
+
+func get_under_object():
+	return under_object
---
+++ menu_ui.gd
@@ -0,0 +1,36 @@
+extends Control
+
+@onready var timer: Timer = $Timer
+@onready var time_left: Label = $timeLeft
+@onready var window: Window = $Window
+@onready var button: Button = $Window/Button
+@onready var label: Label = $Window/Label
+
+var restarted = false
+
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	button.pressed.connect(_on_button_pressed)
+	
+	window.hide()
+	timer.start(7)
+
+
+# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _process(delta: float) -> void:
+	time_left.text = str(timer.time_left).pad_decimals(1)
+	if (timer.time_left <= 0):
+		label.text = "Game Over"
+		window.show()
+		
+	if (get_parent().game_complete == true):
+		label.text = "Good job!"
+		window.show()
+		timer.paused = true
+
+
+func _on_button_pressed() -> void:
+	get_parent().start_game()
+	window.hide()
+	timer.start(7)
+	timer.paused = false
