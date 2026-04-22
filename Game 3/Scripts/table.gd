450b5568c22db136a5d5f786709dd7a08feee9b2
francismontesa <francis.montesa@student.ateneo.edu>
2026-04-22 09:49:39 +0800

    Added timer, win/lose conditions, and mouse controls

--- Game 3/Scripts/table.gd
+++ Game 3/Scripts/table.gd
@@ -2,12 +2,16 @@ extends Area2D
 
 const BOOK = preload("res://Game 3/Scenes/book.tscn")
 
+const MAX_BOOKS = 10
+
 var reserve_books: Array[Node2D] = []
 
-var max_size = 1
+var player_max_capacity = 5
+
+var game_has_ended = false
 
 func _process(delta: float) -> void:
-	if randf() <= 0.005:
+	if randf() <= 0.01 and reserve_books.size() < 10 and !game_has_ended:
 		var new_book = BOOK.instantiate()
 		print("Book Arrived")
 		new_book.position.x = 0
@@ -16,7 +20,7 @@ func _process(delta: float) -> void:
 		reserve_books.append(new_book)
 
 func interact(books: Array[Node2D]) -> Array[Node2D]:
-	if books.size() < max_size:
+	if books.size() < player_max_capacity:
 		var new_book = get_book()
 		if new_book != null:
 			books.append(new_book)
@@ -29,3 +33,6 @@ func get_book() -> Node2D:
 		for book in reserve_books:
 			book.position.y += 5
 		return reserve_books.pop_front()
+		
+func end_game() -> void:
+	game_has_ended = true
