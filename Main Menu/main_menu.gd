38c1e872a36eb798cb5163927ae7f88c88b32865
Leo Sibayan <rafaellosibayan36@gmail.com>
2026-04-25 02:47:56 +0800

    Added tweens to ready screen

--- Main Menu/main_menu.gd
+++ Main Menu/main_menu.gd
@@ -1,5 +1,7 @@
 extends Control
 
+const type_time: int = 13
+
 var title_screen: Control
 var user_screen: Control
 var user_input: LineEdit
@@ -30,13 +32,21 @@ func _process(float) -> void:
 func connect_signals() -> void:
 	title_screen.gui_input.connect(_on_screen_clicked)
 	user_confirm.pressed.connect(_on_confirm_pressed)
+	ready_screen.gui_input.connect(_on_screen_clicked)
+	ready_screen.set_block_signals(true)
 
 
 func _on_screen_clicked(input: InputEvent) -> void:
 	if Input.is_anything_pressed():
-		title_screen.hide()
-		user_screen.show()
-		ready_screen.hide()
+		if title_screen.visible == true:
+			title_screen.hide()
+			user_screen.show()
+			ready_screen.hide()
+		
+		if ready_screen.visible == true:
+			title_screen.hide()
+			user_screen.hide()
+			ready_screen.hide()
 		
 
 func _on_confirm_pressed() -> void:
@@ -44,3 +54,19 @@ func _on_confirm_pressed() -> void:
 	title_screen.hide()
 	user_screen.hide()
 	ready_screen.show()
+	_format_letter_text()
+	await get_tree().create_timer(type_time).timeout
+	ready_screen.set_block_signals(false)
+	
+
+func _format_letter_text() -> void:
+	ready_screen.get_node("LetterText").append_text("Dear %s" % [user_name] + "\n\n")
+	ready_screen.get_node("LetterText").append_text("Following recent updates on the flood control project issues, some of the misused funds have now been recovered. As per your office's functions, you are instructed to make sure that these funds are used properly and for their original purpose. 
+
+Together with your team, you are responsible for using these funds to improve public services and projects as they were meant to be.
+
+You are advised to keep mistakes to a minimum. Errors may result in the need for replacement equipment and additional expenses, which will be charged against the budget. Repeated or excessive mistakes may lead to your removal from the position.
+	")
+	
+	var typewriter = self.create_tween()
+	typewriter.tween_property($ReadyScreen/LetterText, "visible_ratio", 1.0, type_time)
