3cc7d858281fb96485fe9dc880167461df3fad9c
Enzo Pamintuan <enzopamintuan.1013@gmail.com>
2026-04-27 20:12:14 +0800

    Draggable medicines

--- Game 2 Resources/medicine.gd
+++ Game 2 Resources/medicine.gd
@@ -4,18 +4,33 @@ signal placed_on_shelf(category, shelf_name)
 @onready var dispcat = $Label2
 
 var category = ""
-var selected = false
+var dragging = false
 
 func _ready():
 	add_to_group("medicines")
 	connect("input_event", Callable(self, "_on_input_event"))
 
 func _on_input_event(viewport, event, shape_idx):
-	if event is InputEventMouseButton and event.pressed:
-		selected = !selected
-		if selected:
-			print("Selected medicine: %s" % category)
+	if event is InputEventMouseButton:
+		if event.pressed:
+			dragging = true
+		else:
+			dragging = false
+			check_shelf_overlap()
+			
+func _process(delta):
+	if dragging:
+		position = get_global_mouse_position()
+		
+func check_shelf_overlap():
+	var overlapping = get_overlapping_areas()
+	for area in overlapping:
+		if area.is_in_group("shelves"):
+			place_on_shelf(area.name)
+			return
+			
+	queue_free()
 
-func place_on_shelf(shelf_name):
+func place_on_shelf(shelf_name: String):
 	emit_signal("placed_on_shelf", category, shelf_name)
 	queue_free()
