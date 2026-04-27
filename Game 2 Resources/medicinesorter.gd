2a503d9893ccbc1c56b3885458467f09ea7208e2
Enzo Pamintuan <enzopamintuan.1013@gmail.com>
2026-04-27 14:21:04 +0800

    Prototype medsorter for patients ready

--- Game 2 Resources/medicinesorter.gd
+++ Game 2 Resources/medicinesorter.gd
@@ -1,21 +1,69 @@
 extends Node2D
 
-const BRICK = preload("res://Game 1 Resources/brick.tscn")
-@onready var ui: Control = $Control
+var score = 0
+const MEDICINE = preload("res://Game 2 Resources/medicine.tscn")
 
-var on_floor: Array = []
-var on_brick: Array = []
+@onready var ui: Control = $Control
+@onready var scoreboard: Label = $scoreBoard
+@onready var randotime: Timer = $RandomTimer
+@onready var medspawn: Node2D = $MedicineSpawner
+
 var game_complete: bool = false
 
 func start_game() -> void:
+	# Reset state
+	score = 0
 	game_complete = false
+	scoreboard.text = "Score: %d" % score
 
+	# Clear existing medicines
+	for med in get_tree().get_nodes_in_group("medicines"):
+		med.queue_free()
 
-# Called when the node enters the scene tree for the first time.
+	# Restart spawn timer
+	randotime.wait_time = randi_range(1, 3)
+	randotime.start()
+
 func _ready() -> void:
-	pass
+	randotime.wait_time = randi_range(1, 3)
+	randotime.start()
+	scoreboard.text = "Score: %d" % score
 
-# Called every frame. 'delta' is the elapsed time since the previous frame.
+func _on_RandomTimer_timeout() -> void:
+	spawn_medicine()
+	randotime.wait_time = randi_range(1, 3)
+	randotime.start()
+
+func spawn_medicine() -> void:
+	var med = MEDICINE.instantiate()
+	var medcat = ["OTC", "Prescribed", "Critical"]
+	med.category = medcat[randi() % medcat.size()]
+	med.position = medspawn.position
+	add_child(med)
+	med.add_to_group("medicines")
+	med.connect("placed_on_shelf", Callable(self, "_on_medicine_placed"))
+	med.dispcat.text = med.category
+	print("Spawned medicine:", med.category, "at", med.position) # debug line
+
+func _on_medicine_placed(med_category: String, shelf_name: String) -> void:
+	var correct = false
+	if med_category == "OTC" and shelf_name == "ShelfOTC":
+		correct = true
+	elif med_category == "Prescribed" and shelf_name == "ShelfPres":
+		correct = true
+	elif med_category == "Critical" and shelf_name == "ShelfCrit":
+		correct = true
+
+	if correct:
+		score += 500
+	else:
+		score -= 300
+	scoreboard.text = "Score: %d" % score
+
+	# SAMPLE WIN TERMINATOR.
+	if score >= 2500:
+		game_complete = true
+
 func _process(delta: float) -> void:
-	if (!game_complete):
-		pass
+	if game_complete:
+		randotime.stop()
