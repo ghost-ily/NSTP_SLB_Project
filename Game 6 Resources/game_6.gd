a282ae6d2ea42a58a8e22462b31b1d2702ddae13
Leo Sibayan <rafaellosibayan36@gmail.com>
2026-04-28 17:25:16 +0800

    Added game 6

--- Game 6 Resources/game_6.gd
+++ Game 6 Resources/game_6.gd
@@ -1,7 +1,7 @@
 extends Node2D
 
 @onready var trash = preload("res://Game 6 Resources/trash.tscn")
-var all_trash: Array
+var all_trash: Array = []
 
 var service = "Environmental"
 var game_complete: bool = false
@@ -13,17 +13,20 @@ func _ready() -> void:
 
 # Called every frame. 'delta' is the elapsed time since the previous frame.
 func _process(delta: float) -> void:
-	if get_tree().get_nodes_in_group("garbage").is_empty():
+	if all_trash.size() <= 0:
 		game_complete = true
+	for t in all_trash:
+		if t == null:
+			all_trash.erase(t)
 
 
 func start_game():
-	if !get_tree().get_nodes_in_group("garbage").is_empty():
-		for t in get_tree().get_nodes_in_group("garbage"):
+	if all_trash.size() > 0:
+		for t in all_trash:
 			t.queue_free()
 	game_complete = false
 	for i in range(4, randi_range(8, 12)):
 		var inst_trash = trash.instantiate()
-		inst_trash.add_to_group("garbage")
+		all_trash.append(inst_trash)
 		inst_trash.position = Vector2(randi_range(100, 1000), 520)
 		add_child(inst_trash)
--- UI/game_manager.gd
+++ UI/game_manager.gd
@@ -7,14 +7,15 @@ var debug: Window
 var is_new_game: bool
 
 # Use debug_game_scene to test individual games
-var debug_game_scene = load("res://Medical Equipment Game/med_eqp_game.tscn")
+var debug_game_scene = load("res://Game 6 Resources/game_6.tscn")
 var game_1 = load("res://Game 1 Resources/game1.tscn")
 var game_2 = load("res://Game 2 Resources/medicinesorter.tscn")
 var game_3 = load("res://Game 3/Scenes/game_3.tscn")
 var game_4 = load("res://Pothole/Scenes/pothole_game.tscn")
+var game_6 = load("res://Game 6 Resources/game_6.tscn")
 var packed_game: PackedScene
 var current_game
-var all_games: Array = [game_1, game_2, game_3, game_4]
+var all_games: Array = [game_1, game_2, game_3, game_4, game_6]
 
 var infra_funds: int
 var health_funds: int
